d97dddb60d0f80d34d1ec562af9624d90a68d6d6
shikin_mi <espiritu.felino@yandex.ru>
2026-02-24 08:44:11 +0300

    Выделены параметры методов

--- test_add_group.py
+++ test_add_group.py
@@ -13,9 +13,9 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, group_name="asdfsadf", group_header="dfgdsvdf", group_footer="qwefsadc")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -26,31 +26,31 @@ class TestAddGroup(unittest.TestCase):
     def return_to_groups_page(self, wd: WebDriver):
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd: WebDriver):
+    def create_group(self, wd: WebDriver, group_name, group_header, group_footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("asdfsadf")
+        wd.find_element_by_name("group_name").send_keys(group_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dfgdsvdf")
+        wd.find_element_by_name("group_header").send_keys(group_header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("qwefsadc")
+        wd.find_element_by_name("group_footer").send_keys(group_footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
     def open_groups_page(self, wd: WebDriver):
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, wd: WebDriver):
+    def login(self, wd: WebDriver, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_home_page(self, wd: WebDriver):
