c6f77b82ffb11adb8172534a347f5ab2c862d712
shikin_mi <espiritu.felino@yandex.ru>
2026-02-28 18:30:00 +0300

    Рефакторинг и выделение методов теста на добавление контакта

--- application.py
+++ application.py
@@ -53,3 +53,27 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        #init contact creation
+        wd.find_element_by_link_text("add new").click()
+        #fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        #submit contact
+        wd.find_element_by_xpath("//input[19]").click()
+        self.return_home_page()
+
+    def return_home_page(self):
+        self.wd.find_element_by_link_text("home").click()
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,57 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
+
+from application import Application
 from contact import Contact
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-    
-    def test_untitled_test_case(self):
-        wd = self.wd
-        self.login(wd, "admin", "secret")
-        self.create_contact(wd, Contact(first_name='1', last_name="sdtgvbd", home="24532462456", email="fdger5ty4wt2@"))
-        self.logout(wd)
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def logout(self, wd: WebDriver):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_home_page(self, wd: WebDriver):
-        wd.find_element_by_link_text("home").click()
-
-    def create_contact(self, wd: WebDriver, contact):
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_xpath("//input[19]").click()
-        self.return_home_page(wd)
-
-    def login(self, wd: WebDriver, username, password):
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_id("LoginForm").submit()
-
-    def open_home_page(self, wd: WebDriver):
-        wd.get("http://localhost/addressbook/")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(first_name='1', last_name="sdtgvbd", home="24532462456", email="fdger5ty4wt2@"))
+    app.logout()
