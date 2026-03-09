046c9f5499183bd91feb17b8712e274834556104
shikin_mi <espiritu.felino@yandex.ru>
2026-03-09 21:45:19 +0300

    Повышена интеллектуальность методов delete/modify

--- test/test_edit_first_contact.py
+++ test/test_edit_first_contact.py
@@ -1,8 +1,12 @@
 from model.contact import Contact
 
 def test_edit_first_contact_to_empty(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.edit_first_contact(Contact(first_name='', middle_name='', last_name='', home='', email='', company='', title='', address=''))
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(first_name="test"))
     app.contact.edit_first_contact(Contact(first_name='LN', last_name="LN", home="HOME", email="EMAIL@"))
 
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -3,13 +3,21 @@ from model.group import Group
 
 
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="New group"))
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="NAME1", header="HEADER2", footer="FOOTER3"))
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(header="NG"))
 
 def test_edit_first_group_to_empty(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.edit_first_group(Group(name="", header="", footer=""))
