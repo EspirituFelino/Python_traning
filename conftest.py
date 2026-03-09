b78d47f6b6a8fe3974e8e1b6e065a01a37213644
shikin_mi <espiritu.felino@yandex.ru>
2026-03-09 13:38:11 +0300

    Повышена интеллектуальность методов login/logout

--- conftest.py
+++ conftest.py
@@ -10,17 +10,17 @@ def app(request):
 
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
-    elif not fixture.is_valid():
-        fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.ensure_login(username="admin", password="secret")
 
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
-    global fixture
     def fin():
-        fixture.session.logout()
+        fixture.session.ensure_logout()
         fixture.destroy()
     request.addfinalizer(fin)
+    return fixture
