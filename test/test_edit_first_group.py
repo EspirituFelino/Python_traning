656ef294b8ebfbcf882bb9e19cef2f195c5b1f9e
shikin_mi <espiritu.felino@yandex.ru>
2026-03-14 20:46:51 +0300

    Добавлены сравнения списка групп после создания/редактирования групп

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -6,30 +6,35 @@ def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_edit_first_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="NAME1", header="HEADER2", footer="FOOTER3"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
 
-def test_edit_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(header="NG"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-
-def test_edit_first_group_to_empty(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="", header="", footer=""))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_edit_first_group(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.edit_first_group(Group(name="NAME1", header="HEADER2", footer="FOOTER3"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+# def test_edit_first_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.edit_first_group(Group(header="NG"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+# def test_edit_first_group_to_empty(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.edit_first_group(Group(name="", header="", footer=""))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
