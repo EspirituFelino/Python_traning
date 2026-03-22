4d23e7b3077b72f1da6c889bea9af3b3d59e157f
shikin_mi <espiritu.felino@yandex.ru>
2026-03-22 15:29:09 +0300

    Оптимизирована работа со строками

--- fixture/session.py
+++ fixture/session.py
@@ -11,7 +11,7 @@ class SessionHelper:
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
@@ -35,6 +35,9 @@ class SessionHelper:
                 self.logout()
         self.login(username, password)
 
-    def is_logged_in_as(self, username):
+    def get_logged_username(self):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//div[@id='top']/form/b").text == "("+username+")"
+        return wd.find_element_by_xpath("//div[@id='top']/form/b").text[1:-1]
+
+    def is_logged_in_as(self, username):
+        return self.get_logged_username() == username
