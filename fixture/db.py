5b73d30c0123379f341cd86bfae2a01f63cdd567
shikin_mi <espiritu.felino@yandex.ru>
2026-04-07 22:29:07 +0300

    Добавлен тест для сравнения списка групп UI/DB

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,7 @@
 import pymysql.cursors
 
+from model.group import Group
+
 
 class DbFixture:
     def __init__(self, host, name, user, password):
@@ -11,3 +13,15 @@ class DbFixture:
 
     def destroy(self):
         self.connection.close()
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute('select group_id, group_name, group_header, group_footer from group_list')
+            for row in cursor:
+                id, name, header, footer = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+            return group_list
