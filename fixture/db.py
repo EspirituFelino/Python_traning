782d9ae1f0880f749ecdf0c2c63026e61861e828
shikin_mi <espiritu.felino@yandex.ru>
2026-04-07 23:46:03 +0300

    Добавлен метод получения списка контактов из DB

--- fixture/db.py
+++ fixture/db.py
@@ -1,5 +1,6 @@
 import pymysql.cursors
 
+from model.contact import Contact
 from model.group import Group
 
 
@@ -25,3 +26,15 @@ class DbFixture:
         finally:
             cursor.close()
             return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute('select id, firstname, lastname from addressbook') # where deprecated="0000-00-00 00:00:00"')
+            for row in cursor:
+                id, firstname, lastname = row
+                contact_list.append(Contact(id=str(id), first_name=firstname, last_name=lastname))
+        finally:
+            cursor.close()
+            return contact_list
