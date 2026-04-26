44a82aaa9ce0ab1965d35e2662a0e26132f46859
shikin_mi <espiritu.felino@yandex.ru>
2026-04-26 14:06:24 +0300

    Добавлены BDD тесты для добавления и удаления групп

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,11 @@
+from pytest_bdd import scenario
+from bdd.group_steps import *
+
+
+@scenario('groups.feature', 'Add a new group')
+def test_add_new_group():
+    pass
+
+@scenario('groups.feature', 'Delete some group')
+def test_delete_some_group():
+    pass
